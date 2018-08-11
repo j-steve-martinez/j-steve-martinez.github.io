28c84b875eb9422a39745fd0a2d36d2c4d28092f
J. Steve Martinez <j.steve.martinez@gmail.com>
2018-08-11 14:23:17 -0700

    added d3 lib

--- javascript/main.js
+++ javascript/main.js
@@ -13,66 +13,38 @@ window.onload = () => {
     // script.type = "text/javascript";
     // script.src = "/javascript/tic_tac_toe.js";
     // document.body.appendChild(script);
-    // setHtml("simon");
+    setHtml("main");
 }
 
-function setHtml() {
+function setHtml(id) {
     /**
      * get the root element
+     // var root = document.getElementsByTagName("body")["0"];
      */
-    var root = document.getElementById("simon");
+    var root = document.getElementById(id);
     root.innerText = "";
 
     /**
-     * Add big buttons
+     * Add Content
      */
-    var bigButtons = {
-        1: "quarter quarter-tl",
-        2: "quarter quarter-tr",
-        3: "quarter quarter-bl",
-        4: "quarter quarter-tr"
+    var contents = {
+        "welcome": "page-header",
+        "content": "page-content"
     }
-    var bigBtnWrap = document.createElement("div");
-    Object.entries(bigButtons).forEach((arrayS, count) => {
-        // console.log(arrayS[0]);
-        // console.log(arrayS[1]);
+    var content = document.createElement("div");
+    Object.entries(contents).forEach((entries, count) => {
+        // console.log(entries[0]);
+        // console.log(entries[1]);
         // console.log(count);
         // console.log(y);
-        var tmp = document.createElement("div");
-        tmp.id = arrayS[0];
-        tmp.className = arrayS[1];
-        bigBtnWrap.appendChild(tmp);
+        var element = document.createElement("div");
+        element.id = entries[0];
+        element.className = entries[1];
+        if (entries[0] == "welcome") {
+            element.innerText = "Welcome";
+        }
+        content.appendChild(element);
     });
 
-
-    var ctrlBtns = {
-        "start-c": "small-circle",
-        "strict-c": "small-circle",
-        "reset-c": "small-circle",
-        "start": "label",
-        "strict": "label",
-        "reset": "label",
-        "power": "none",
-        "power-label": "none",
-        "counter": "none"
-    }
-    var ctrlBtnsWrap = document.createElement("div");
-    Object.entries(ctrlBtns).forEach((arrayS, count) => {
-        // console.log(arrayS[0]);
-        // console.log(arrayS[1]);
-        // console.log(count);
-        
-        var tmp = document.createElement("div");
-        tmp.id = arrayS[0];
-        tmp.className = arrayS[1];
-        if (arrayS[1] == "label") {
-            tmp.innerText = arrayS[1].toUpperCase();
-        }
-        if (arrayS[0] == "counter") {
-            tmp.innerText = "--";
-        }
-        ctrlBtnsWrap.appendChild(tmp);
-    });
-    root.appendChild(ctrlBtnsWrap);
-    root.appendChild(bigBtnWrap);
+    root.appendChild(content);
 }
